7c7c7adc75c70816b4980e9d1dbc64427b137d79
stephcl <steph.c.lawrence@gmail.com>
2020-07-26 16:44:32 -0400

    beautify code

--- code.ts
+++ code.ts
@@ -1,4 +1,4 @@
-if (figma.currentPage.selection.length <= 0){
+if (figma.currentPage.selection.length <= 0) {
   figma.closePlugin('Please select a Rectanle, Ellipse, Polygon, Frame or Group before running this plugin')
 }
 
@@ -16,13 +16,13 @@ ref.sort(function (a, b) {
 // TODO: turn this into an dropdown option
 // ref.reverse()
 
-ref.forEach((layer:any) => {
+ref.forEach((layer: any) => {
 
   // make sure it's a vector
   if (layer.type === "RECTANGLE" || layer.type === "ELLIPSE" || layer.type === "POLYGON" || layer.type === "VECTOR") {
-    
-    if(!layer.fillStyleId){
-      
+
+    if (!layer.fillStyleId) {
+
       //creating the paint style
       var newStyle = figma.createPaintStyle()
       var hex = findTheHEX(layer.fills[0].color.r, layer.fills[0].color.g, layer.fills[0].color.b)
@@ -51,25 +51,25 @@ ref.forEach((layer:any) => {
     } else {
       ignoredCounter++
     }
-    
+
   } else if (layer.type === "GROUP" || layer.type === "FRAME") {
-   // console.log(layer.children);
-   // find child layers that are not another group or frame
+    // console.log(layer.children);
+    // find child layers that are not another group or frame
     const colorlayers = layer.findAll(child => child.type === "RECTANGLE" || child.type === "ELLIPSE" || child.type === "POLYGON" || child.type === "VECTOR")
     console.log(colorlayers);
-    if (colorlayers.length <=0) {
+    if (colorlayers.length <= 0) {
       figma.closePlugin('Please select a Group or Frame that has a Rectanle, Ellipse, Polygon, or Vector.')
     } else {
       //needs to be for each color in selected
-      colorlayers.forEach(function(child){
+      colorlayers.forEach(function (child) {
         //console.log(child);
         //now add colors from those layers if they don't already have a color style applied
-        if(!child.fillStyleId){
-        //creating the paint style
+        if (!child.fillStyleId) {
+          //creating the paint style
           var newStyle = figma.createPaintStyle()
           var hex = findTheHEX(child.fills[0].color.r, child.fills[0].color.g, child.fills[0].color.b)
 
-        //naming the paint style with the layer name
+          //naming the paint style with the layer name
           newStyle.name = child.name
           newStyle.description = hex.toUpperCase()
 
@@ -89,16 +89,16 @@ ref.forEach((layer:any) => {
 
           // console log the output
           console.log('🎉 Created style ' + child.name)
-          
 
-          } else {
+
+        } else {
           ignoredCounter++
-          }
-      }) 
+        }
+      })
     }
-  
 
-  
+
+
   } else {
     figma.closePlugin('Please select a Rectanle, Ellipse, Polygon, Vector, Frame, or Group before running this plugin')
   }
@@ -106,13 +106,13 @@ ref.forEach((layer:any) => {
 
 figma.currentPage.selection = []
 
-if(ignoredCounter > 0){
+if (ignoredCounter > 0) {
   figma.closePlugin('⚠️ Warning: ' + ignoredCounter + ' color style(s) already exist and can\'t be added')
 } else {
   figma.closePlugin();
 }
 
-function findTheHEX(red:any, green:any, blue:any) {
+function findTheHEX(red: any, green: any, blue: any) {
   var redHEX = rgbToHex(red)
   var greenHEX = rgbToHex(green)
   var blueHEX = rgbToHex(blue)
@@ -120,7 +120,7 @@ function findTheHEX(red:any, green:any, blue:any) {
   return redHEX + greenHEX + blueHEX
 }
 
-function rgbToHex(rgb:any) {
+function rgbToHex(rgb: any) {
   rgb = Math.floor(rgb * 255)
   var hex = Number(rgb).toString(16)
   if (hex.length < 2) {
